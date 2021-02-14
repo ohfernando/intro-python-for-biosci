782ac14f588602d0185e203e658feea5d0515451
ohfernando <78933840+ohfernando@users.noreply.github.com>
2021-02-14 16:31:18 -0500

    Update ex7.py

--- exercise7/ex7.py
+++ exercise7/ex7.py
@@ -1,12 +1,12 @@
-from ete3 import NCBITaxa
+#from ete3 import NCBITaxa
 import sys
 import taxopy
-from taxonomy_ranks import TaxonomyRanks
-try:
-    ncbi = NCBITaxa()
+#from taxonomy_ranks import TaxonomyRanks
+#try:
+    #ncbi = NCBITaxa()
     #ncbi.update_taxonomy_database()
-except:
-    print('ncbi error')
+#except:
+#    print('ncbi error')
 
 
 def getlin(org):
@@ -14,8 +14,15 @@ def getlin(org):
     a=rank_taxon.get_lineage_taxids_and_taxanames()
     print(a)
 
+
 def printfunc(x,a):
     print(x + ' is ' +a[x])
+
+def finalout(d,orgid):
+    list1=['family','order','genus']
+    print(orgid + ":")
+    for i in list1:
+        print(str(i) + "-" + str(d[i]))
     
 
 try:
@@ -26,20 +33,17 @@ except:
     #except:
         #print('taxopy error, try again!')
 
-taxaidfile=open(sys.argv[1],"r")
-for line in taxaidfile:
-    stringoftaxids=str(line)
+#taxaidfile=open(sys.argv[1],"r")
+#for line in taxaidfile:
+ #   stringoftaxids=str(line)
 #taxaids=stringoftaxids.split()
-#taxaids=['3702','3712']
+taxaids=['3702','3712']
 
 
 org1= taxopy.Taxon(taxaids[0], taxdb)
 org2 = taxopy.Taxon(taxaids[1], taxdb)
 print(str(taxopy.find_lca([org1, org2], taxdb)) + ' is LCA')
-print(org1.rank_name_dictionary)
-print(org2.rank_name_dictionary)
-rank_taxon = TaxonomyRanks(org1)
-a=rank_taxon.get_lineage_taxids_and_taxanames()
-printfunc('family',a)
-printfunc('genus',a)
-printfunc('order',a)
+org1dict=org1.rank_name_dictionary
+org2dict=org2.rank_name_dictionary
+finalout(org1dict,taxaids[0])
+finalout(org2dict,taxaids[1])
